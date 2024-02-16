639188a89a2a1a1fbd31edfdb5864a28c75b9213
Aso Naoki <aso@live2d.com>
2024-02-16 13:54:51 +0900

    Update sound_effect.js
    * 白飛びしないよう調整
    * スクリプト終了時に色と位置を戻す
    * 無駄な変数を削除

--- models/01arisa/scripts/sound_effect.js
+++ models/01arisa/scripts/sound_effect.js
@@ -1,6 +1,5 @@
 console.log("sound effect")
 
-let lastParams
 function start(model) {
     for (let param of model.parameters) {
         console.log(param.name, param.id)
@@ -9,15 +8,18 @@ function start(model) {
 }
 
 function update(model, params) {
-    lastParams = params
-
     let value = params.LipSyncVolume
-    let color = Live.rgba(0.8 + value, 0.8 + value, 0.8 + value, 1)
+    let cv = Math.min(0.8 + value, 1)
+    let color = Live.rgba(cv, cv, cv, 1)
     model.multiplyColor = color
     if (value > 0.1) {
-        model.move({ Absolute: true, X: 0, Y: Math.min(0, value - 0.5), Delay: 0.1 })
+        model.move({ Absolute: true, X: 0, Y: Math.min(-0.3, value - 0.5), Delay: 0.1 })
     } else {
         model.move({ Absolute: true, X: 0, Y: -0.5, Delay: 0.1 })
-
     }
+}
+
+function onDisable(model) {
+    model.multiplyColor = Live.rgba(1, 1, 1, 1)
+    model.move({ Absolute: true, X: 0, Y: -0.5, Delay: 0.1 })
 }
