5cc77ffc3edd63fff91fb729deb4912ca377b236
baba-at-live2d-com <153706253+baba-at-live2d-com@users.noreply.github.com>
2024-11-22 10:56:20 +0900

    add cameramap.js

---
+++ scripts/cameramap.js
@@ -0,0 +1,10 @@
+﻿console.log("cameramap script")
+
+function update() {
+    let params = model.liveParameters
+    let layer = scene.effect.background.layer("CameraMap")
+    if (layer) {
+        layer.setParameter("zurashiX", - params.Yaw / 90 * 0.3)
+        layer.setParameter("zurashiY", params.Pitch / 90 * 0.2)
+    }
+}
